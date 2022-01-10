2101b54bfa947458e8fee9bc3b775d363d57c2b0
MertPlc <mertpalacioglu@gmail.com>
2022-01-11 00:01:01 +0300

    updating the room

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
 // dom queries
 const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
-const newNameForm = document.querySelector('.new-name')
-const updateMssg = document.querySelector('.update-mssg')
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
 
 // ad a new chat
 newChatForm.addEventListener("submit", (e) => {
@@ -24,6 +25,15 @@ newNameForm.addEventListener('submit', e=> {
   // show then hide the update message
   updateMssg.innerText = `Your name was updated to ${newName}`;
   setTimeout(() => updateMssg.innerText = '', 3000);
+});
+
+// update the chat room
+rooms.addEventListener('click', e => {
+  if(e.target.tagName === 'BUTTON') {
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute('id'));
+    chatroom.getChats(chat => chatUI.render(chat));
+  }
 });
 
 // check local storage for a name
